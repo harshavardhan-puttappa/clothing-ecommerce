ad53f35eb13d820a836bd59ff1b254e62857e77e
puttappa.harshavardhan@gmail.com <puttappa.harshavardhan@gmail.com>
2020-02-08 17:47:51 +0530

    broken homepage component into smaller components , finished styling our menu-item component

---
+++ src/components/directory/Directory.jsx
@@ -0,0 +1,49 @@
+import React, { Component } from "react";
+import "./directory.styles.scss";
+import MenuItem from "../menu-item/MenuItem";
+
+class Directory extends Component {
+  state = {
+    sections: [
+      {
+        title: "HATS",
+        imageUrl: "https://i.ibb.co/cvpntL1/hats.png",
+        id: 1
+      },
+      {
+        title: "JACKETS",
+        imageUrl: "https://i.ibb.co/px2tCc3/jackets.png",
+        id: 2
+      },
+      {
+        title: "SNEAKERS",
+        imageUrl: "https://i.ibb.co/0jqHpnp/sneakers.png",
+        id: 3
+      },
+      {
+        title: "MENS",
+        imageUrl: "https://i.ibb.co/R70vBrQ/men.png",
+        id: 4,
+        size: "large"
+      },
+      {
+        title: "WOMENS",
+        imageUrl: "https://i.ibb.co/GCCdy8t/womens.png",
+        id: 5,
+        size: "large"
+      }
+    ]
+  };
+  render() {
+    const { sections } = this.state;
+    return (
+      <div className="directory-menu">
+        {sections.map(section => (
+          <MenuItem key={section.id} section={section} />
+        ))}
+      </div>
+    );
+  }
+}
+
+export default Directory;
