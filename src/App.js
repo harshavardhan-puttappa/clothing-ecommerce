dcc2d8aaf54613b99ca3d6109cb4c8288b90daa4
puttappa.harshavardhan@gmail.com <puttappa.harshavardhan@gmail.com>
2020-02-09 14:59:28 +0530

    added shoppage, collection-preview and collection-item component

--- src/App.js
+++ src/App.js
@@ -2,21 +2,15 @@ import React, { Fragment } from "react";
 import "./App.css";
 import HomePage from "./pages/homepage/HomePage";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import ShopPage from "./pages/shoppage/ShopPage";
 
-const Hats = () => {
-  return (
-    <div>
-      <h1>HATS PAGE</h1>
-    </div>
-  );
-};
 const App = () => {
   return (
     <Router>
       <Fragment>
         <Switch>
           <Route exact path="/" component={HomePage} />
-          <Route path="/hats" component={Hats} />
+          <Route path="/shop" component={ShopPage} />
         </Switch>
       </Fragment>
     </Router>
